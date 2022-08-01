42908797a40729823279c9016545bc2799d3e02e
shivanineela <109790213+shivanineela@users.noreply.github.com>
2022-08-01 21:08:02 +0530

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,124 @@
+# a='computer'
+# print(a.upper())
+
+# print('computer'.upper())
+# print('COMPUTER'.lower())
+# print('COMPUTER'.casefold())
+# print('System'.isupper())
+# print('SYSTEM'.isupper())
+# print('system'.islower())
+# print('sySteM'.islower())
+# print('cOmPuTEr'.swapcase())
+# print('heLLo WOrLd'.capitalize())
+# print('heLLo wOrLd'.title())
+# print('Hello World'.istitle())
+# print('Hello world'.istitle())
+# print('Note Book'.startswith('B'))
+# print('Note Book'.startswith('n'))
+# print('Note Book'.startswith('N'))
+# print('Note Book'.startswith('No'))
+# print('Note Book'.endswith('No'))
+# print('Note Book'.endswith('k'))
+# print('Note Book'.endswith('Book'))
+# print(len('calendar'))
+# print('calendar'.find('d'))
+# print('calendar'.find('c'))
+# print('calendar'.find('C'))
+
+# print('hi'.isidentifier())
+# print('_hi'.isidentifier())
+# print('5hi'.isidentifier())
+
+# a="programming"
+# print(a.zfill(11))
+# print(a.zfill(9))
+# print(a.zfill(14))
+
+# name="python programming"
+# print(name.center(25))
+# print(name.center(25,'^'))
+# print(name.center(50,'*'))
+
+# a="computer"
+# print(type(a))
+# n=a.encode()
+# print(n)
+# print(a.decode())
+
+# s='hello world'
+# print(s.endswith(''))
+# print(s.endswith(' '))
+# print(s.endswith('ld'))
+
+# n='hello world'
+# print(n.startswith(''))
+# print(n.startswith(' '))
+# print(n.startswith('h'))
+
+# x=' hello world'
+# print(x.startswith(''))
+# print(x.startswith(' '))
+# print(x.startswith('h'))
+
+# a=''
+# print(a.isspace())
+# b='           '
+# print(b.isspace())
+# c='   .  '
+# print(c.isspace())
+# d='python'
+# print(d.isspace())
+
+# q="methodology"
+# print(q.index('l'))
+# print(q.index('o',3))
+# print(q.index('o',5))
+# print(q.index('o'))
+# print(q.index('o',4))
+# print(q.rindex('g'))
+# print(q.index('o'))
+# print(q.rindex('o'))
+# print(q.index('b'))
+
+# r="hello world programming"
+# print(r.index('u'))
+# print(r.index('o'))
+# print(len(r))
+# print(r.rindex('g'))
+
+# r='hello world programming'
+# print(len(r))
+# print(len('hello world programming'))
+
+# g="python7 programming"
+# print(g.find('m'))
+# print(g.find('o'))
+# print(g.find('s'))
+# print(len(g))
+# print(g.rfind('m'))
+
+# v='hello python'
+# print(v.count('o'))
+# print(v.count('p'))
+# print(v.count('g'))
+
+# j="python programming"
+# print(j.replace('programming','course'))
+
+# w="course python programming course python"
+# print(w.replace('python',str(678)))
+# print(w.replace('python','678'))
+# print(w.replace('o','s',3))
+# print(w.replace('o','s',5))
+# print(w.replace('o','s',(5)))
+
+# d="             python course      "
+# print(d.strip())
+# print(d.lstrip())
+# print(d.rstrip())
+# print(len(d))
+# e=d.strip()
+# print(len(e))
+
+# f="      hello everyone     ".strip()
+# print(len(f))
